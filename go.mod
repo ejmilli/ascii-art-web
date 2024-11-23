b77f7b68ee56dbea22dec06a18c345cd32c2a6cf
cmbigk <bigk89865@gmail.com>
2024-11-23 22:54:20 +0200

    still needs to do many thing for the project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ascrii-art-web
+
+go 1.23.1
--- script.js
+++
@@ -1,9 +0,0 @@
-function updateCard() {
-  var bgColor = document.getElementById("backgroundColor").value;
-  var borderColor = document.getElementById("borderColor").value;
-  var customText = document.getElementById("customText").value;
-
-  document.getElementById("customCard").style.backgroundColor = bgColor;
-  document.getElementById("customCard").style.borderColor = borderColor;
-  document.getElementById("cardText").innerText = customText;
-}
