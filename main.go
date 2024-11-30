b266e1560bc5b4f81aac4681f1e7e93549863b1d
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-30 22:11:54 +0200

    done a few things in my code

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
+
+var tpl *template.Template
+
+func init() {
+	// Parse the template during initialization so it's available globally
+	tpl = template.Must(template.ParseFiles("templates/index.html"))}
+
 func HandleFunc(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprint(w, "Start of our ascii web project")
+	// Render the template when the endpoint is hit
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	}
 }
-
 func main() {
+
+
 
 	http.HandleFunc("/", HandleFunc)
 	 http.ListenAndServe(":8080", nil)
