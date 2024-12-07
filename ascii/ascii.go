273d48f5aa5b7ee5935c918318ae4aa8eece8792
cmbigk <bigk89865@gmail.com>
2024-12-07 13:54:12 +0200

    pushing main from bigK

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -7,8 +7,22 @@ import (
 	"strings"
 )
 
+func CleanInput(text string) string {
+	return strings.ReplaceAll(text, "\r", "")
+}
+
 // GenerateASCIIArt generates ASCII art for the given text and template.
-func GenerateASCIIArt(text, template string) (string, error) {
+func GenerateASCIIArt(text, template string) (string, int) {
+
+	text = CleanInput(text)
+	// Validate input for unprintable ASCII characters
+	for _, char := range text {
+		if char < 32 && char > 126 {
+			return fmt.Sprintf("Error: Unprintable character '%c' found", char), 400
+		}
+
+	}
+
 	templates := map[string]string{
 		"standard":   "./ascii/txt/standard.txt",
 		"shadow":     "./ascii/txt/shadow.txt",
@@ -17,15 +31,16 @@ func GenerateASCIIArt(text, template string) (string, error) {
 
 	templatePath, exists := templates[template]
 	if !exists {
-		return "", fmt.Errorf("template not found: %s", template)
+		return fmt.Sprintf("Error: Template '%s' not found", template), 400
 	}
 
 	asciiMap := LoadTemplate(templatePath)
 	if asciiMap == nil {
-		return "", fmt.Errorf("failed to load template: %s", templatePath)
+		return fmt.Sprintf("Error: Failed to load template '%s'", templatePath), 500
 	}
 
-	return RenderASCII(asciiMap, text), nil
+	asciiArt := RenderASCII(asciiMap, text)
+	return asciiArt, 200
 }
 
 // RenderASCII generates the ASCII art string.
@@ -34,20 +49,25 @@ func RenderASCII(asciiMap map[rune][]string, text string) string {
 	lines := SplitTextByLines(ParseEscapeSequences(text))
 
 	for _, line := range lines {
-		for i := 0; i < 8; i++ {
-			for _, char := range line {
-				asciiLines, exists := asciiMap[char]
-				if !exists {
-					if !exists {
-						fmt.Printf("Character not found: '%c'\n", char)
-					}
+		// Prepare an array for each line of ASCII art (8 lines)
+		asciiArtLines := make([]string, 8)
 
-					result.WriteString(fmt.Sprintf("Error: '%c' not found\n", char))
-					continue
-				}
-				result.WriteString(asciiLines[i])
+		for _, char := range line {
+			asciiLines, exists := asciiMap[char]
+			if !exists {
+				// Log the error only once per character and skip processing
+				result.WriteString(fmt.Sprintf("Error: '%c' not found\n", char))
+				continue
 			}
-			result.WriteString("\n")
+			// Append ASCII lines for this character to the corresponding line in the result
+			for i := 0; i < 8; i++ {
+				asciiArtLines[i] += asciiLines[i]
+			}
+		}
+
+		// Add the complete ASCII lines for the current input line
+		for _, asciiLine := range asciiArtLines {
+			result.WriteString(asciiLine + "\n")
 		}
 	}
 	return result.String()
@@ -77,8 +97,10 @@ func LoadTemplate(filePath string) map[rune][]string {
 		} else {
 			lines = append(lines, line)
 		}
+
 	}
 	return asciiMap
+
 }
 
 func ParseEscapeSequences(input string) string {
